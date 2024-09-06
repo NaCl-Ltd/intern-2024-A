067af7307d3564eaa75fe8a8fdba61db56bbb65d
asuka-0104 <t5424037@fukuyama-u.ac.jp>
2024-09-06 11:57:20 +0900

    fix:多言語化のコードの簡略化

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,8 @@
 require_relative "boot"
 require "rails/all"
 Bundler.require(*Rails.groups)
-
 module SampleApp
   class Application < Rails::Application
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb, yml}').to_s]
-    I18n.available_locales = [:en, :ja]
     I18n.default_locale = :ja
   end
 end
