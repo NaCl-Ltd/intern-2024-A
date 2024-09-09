417f0cb82e287297a19c990c61926bcf2369fc22
asuka-0104 <t5424037@fukuyama-u.ac.jp>
2024-09-09 13:52:10 +0900

    wip:Newリンクの追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,6 +14,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get help" do
+    get new_path
+    assert_response :success
+    assert_select "title", "New | Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get about" do
     get about_path
     assert_response :success
