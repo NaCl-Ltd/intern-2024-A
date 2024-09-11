7b178d625c35f34dcd2f3f84c7986bfd6de04b03
Rengemaru <t5424035@fukuyama-u.ac.jp>
2024-09-11 12:07:29 +0900

    feat:#12 ユーザー検索機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_05_054036) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_10_064420) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_05_054036) do
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.text "introduction"
+    t.text "search"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
